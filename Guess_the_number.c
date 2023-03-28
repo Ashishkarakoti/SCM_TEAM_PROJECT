4e9ff897c4c070ac55e05e2d61166b87dafda7f3
aryansalvatore <123055427+aryansalvatore@users.noreply.github.com>
2023-03-27 21:49:56 -0700

    Update Guess_the_number.c

--- Guess_the_number.c
+++ Guess_the_number.c
@@ -1,20 +1,20 @@
-#include <stdio.h> 
-#include <stdlib.h> 
-#include <time.h> 
-int main() { 
+#include  <stdio.h> 
+#include  <stdlib.h> 
+#include  <time.h> 
+int main()  { 
     
     int guess, num, count = 0; 
     char choice; 
     srand(time(NULL)); // seed the random number generator 
     do {
         num = rand() % 100 + 1;
-// generate a random number between 1 and 100
-        count = 0; 
-        printf("I'm thinking of a number between 1 and 100. Guess what it is!\n"); 
-        do {
-            printf("Enter your guess : "); 
-            scanf("%d", &guess); 
-            count++; 
+//  generate a random number between 1 and 100
+         count = 0; 
+         printf("I'm thinking of a number between 1 and 100. Guess what it is!\n"); 
+         do {
+             printf("Enter your guess : "); 
+             scanf("%d", &guess); 
+             count++; 
             if (guess > num) { 
                 printf("Too high! Guess again.\n"); 
             } else if (guess < num) { 
