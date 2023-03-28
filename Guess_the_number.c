83a113af994621433eca227f051e79b7673e86b0
Ayush <123055432+AyushSalvatotre@users.noreply.github.com>
2023-03-28 10:35:42 +0530

    Update Guess_the_number.c

--- Guess_the_number.c
+++ Guess_the_number.c
@@ -16,9 +16,9 @@ int main()
     do {
         num = rand()  %  100 + 1;  
            // generate a random number between 1 and 100
-        count = 0; 
-        printf("I'm thinking of a number between 1 and 100. Guess what it is ? \n"); 
-        do {
+                                                           count = 0; 
+                                                          printf("I'm thinking of a number between 1 and 100. Guess what it is ? \n"); 
+                                                     do {
                 printf("Enter    your    guess : "); 
                 scanf("%d", &guess); 
                 count++; 
